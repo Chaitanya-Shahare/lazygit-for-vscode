8a22e1041a08ecace7865e35223b59575fbb8e7e
fulecorafa <ra.pha@live.com>
2024-01-12 22:53:35 -0300

    refactor: extract new lazygit instance into its own function

--- src/extension.ts
+++ src/extension.ts
@@ -3,26 +3,28 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "lazygit.openLazygit",
-    async () => {
-      // Always create a new terminal
-      let terminal = vscode.window.createTerminal();
-
-      terminal.sendText("lazygit && exit");
-      terminal.show();
-
-      // Move the terminal to the editor area
-      await vscode.commands.executeCommand(
-        "workbench.action.terminal.moveToEditor"
-      );
-
-      // Move focus back to the editor view
-      await vscode.commands.executeCommand(
-        "workbench.action.focusActiveEditorGroup"
-      );
-    }
+    newLazygitInstance
   );
 
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+async function newLazygitInstance() {
+  // Always create a new terminal
+  let terminal = vscode.window.createTerminal();
+
+  terminal.sendText("lazygit && exit");
+  terminal.show();
+
+  // Move the terminal to the editor area
+  await vscode.commands.executeCommand(
+    "workbench.action.terminal.moveToEditor"
+  );
+
+  // Move focus back to the editor view
+  await vscode.commands.executeCommand(
+    "workbench.action.focusActiveEditorGroup"
+  );
+}
+
+export function deactivate() { }
